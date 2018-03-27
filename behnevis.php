b437915461cadaced166a16003092a28bb72cf14
Nima HeydariNasab <nima.heydari79@yahoo.com>
2018-03-27 12:08:21 +0000

    Apply fixes from StyleCI

--- behnevis.php
+++ behnevis.php
@@ -8,13 +8,12 @@
 */
 
 header('Content-Type: application/json');
-if(isset($_GET["text"])) {
-  $ch = curl_init("http://www.behnevis.com/php/convert.php");
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, ["farsi" => $_GET["text"]]);
-  $result = curl_exec($ch);
-  curl_close($ch);
-  exit('{"persian":'.json_encode($result).'}');
+if (isset($_GET['text'])) {
+    $ch = curl_init('http://www.behnevis.com/php/convert.php');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, ['farsi' => $_GET['text']]);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    exit('{"persian":'.json_encode($result).'}');
 }
 exit('{"error":"parameter url is required"}');
-?>
